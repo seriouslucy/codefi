a716eac8e3841ff6e12b2c8853edf3c841d93fd2
seriouslucy <seriouslucy@outlook.com>
2025-02-03 21:22:53 -0600

    modified
    new practice pages added!!!

---
+++ class-practice/09-advanced-functions/script.js
@@ -0,0 +1,73 @@
+// console.log('connected');
+
+// const double = (number) => {
+// console.log(number * 2)    
+// }
+// double(10);
+
+
+// const applyDiscount = (prices, discount, callback) => {
+//     callback(prices, discount);
+// }
+
+// const discountFunction = (prices, discount) => {
+//     let discountPrices = prices.map((p) => p - (p * discount));
+//     console.log(discountPrices);
+// };
+// applyDiscount([10, 20, 30], .10, discountFunction);
+
+// const squareAndPrint = (numbers) => {
+//  const squaredNumbers = numbers.map((num) => num * num);
+//  console.log(squaredNumbers);
+// };
+
+// squareAndPrint([2, 4, 5]);
+
+
+
+// const oddNumbers = (nums) => {
+//     let oddArray = nums.filter((num) => num % 2 !== 0);
+//     console.log(oddArray);
+// }
+
+// oddNumbers([1, 2, 3, 4, 5, 6, 7]);
+
+// const startTask = () => {
+//     setTimeout (() => {
+//         console.log('Task completed!');
+//     }, 5000);
+//     setInterval (() => {
+//         console.log('Still waiting...')
+//     }, 2000);
+// };
+
+// startTask();
+
+// const repeatNum = (nums) => {
+// let duplicateArray = [];
+//     for (let index = 0; nums.length > index; index++) {
+//     let currentNumber = nums[index]
+   
+//     for (let j = 0; j < nums.length; j++) {
+//         let currentIndex = nums[j];
+       
+//     if (currentNumber === currentIndex && index !== j) {
+//         duplicateArray.push(currentNumber);
+//     } 
+
+// } 
+// };
+
+// console.log(duplicateArray);
+// }
+
+// repeatNum([1, 2, 4, 5, 4, 2, 6]);
+
+
+
+
+
+
+
+
+
