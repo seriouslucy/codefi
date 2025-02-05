732b7f7bb918497a63065e4909f303ab62fd3293
seriouslucy <seriouslucy@outlook.com>
2025-02-04 19:20:10 -0600

    new
    created new index in random-stuff folder, added new images and sounds

---
+++ homework/random-stuff/scripts/script.js
@@ -0,0 +1,18 @@
+// alert('I hope this hurts');
+
+window.addEventListener("mouseover", myFunction1);
+window.addEventListener("mouseout", myFunction2);
+window.addEventListener("click", myFunction3)
+
+function myFunction1() {
+  document.getElementById("idea").innerHTML += "I HOPE THIS HURTS "
+}
+
+function myFunction2() {
+  document.getElementById("idea").innerHTML += "I HOPE THIS HURTS "
+}
+
+function myFunction3() {
+    document.getElementById("idea").innerHTML += "TAKE RESPONSIBILITY "
+}
+
---
+++ homework/random-stuff/scripts/sunset.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.js-next');
+
+btn.addEventListener('click', clickHandler);
+
+function clickHandler() {
+    window.location.href = "mouthwashing.html";
+};
