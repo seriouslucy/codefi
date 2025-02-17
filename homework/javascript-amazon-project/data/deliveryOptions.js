a3b94abc91804fd811c8a4f987740be3e0a37dc0
seriouslucy <seriouslucy@outlook.com>
2025-02-16 19:22:23 -0600

    modified
    save data and generate HTML for payment summary

--- homework/javascript-amazon-project/data/deliveryOptions.js
+++ homework/javascript-amazon-project/data/deliveryOptions.js
@@ -11,4 +11,17 @@ export const deliveryOptions = [{
     id: '3',
     deliveryDays: 1,
     priceCents: 999
-}]
+}];
+
+export function getDeliveryOption(deliveryOptionId) {
+     let deliveryOption;
+    
+          deliveryOptions.forEach((option) => {
+            if (option.id === deliveryOptionId) {
+              deliveryOption = option;
+            }
+          });
+
+    return deliveryOption || deliveryOptions[0];
+    
+}
